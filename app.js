b7688b69ac0b4e8d42b2339ab6c50ca6d7557dc8
Jeffry Jimenez <codingfulltime@gmail.com>
2020-08-24 23:21:04 -0600

    added bread animation

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const mainNav = document.querySelector('.main-nav');
 const navLink = document.querySelectorAll('.nav-link');
 const lineHamburger = document.querySelectorAll('.line');
 
+
+
 // close and open Navbar with Hamburger menu
 menuIcon.addEventListener('click', (e) => {
     mainNav.classList.toggle('change');
@@ -26,3 +28,28 @@ window.onscroll = function() {
     }
     prevScrollpos = currentScrollPos;
 }
+
+// Bread Animation
+const breadContainer = document.querySelector('.bread-container');
+function breadAnimation(){
+
+    let breadItems;
+    for(breadItems = 0; breadItems <= 500; breadItems++) {
+        setTimeout(function (){
+            let oneBread = document.createElement('div');
+            oneBread.classList.add('bread');
+            oneBread.classList.add(`bread-`+ breadItems);
+            oneBread.style.animationDelay = `${Math.floor(Math.random() * (5-1) + (1) )}s`;
+            oneBread.style.width = `${Math.floor(Math.random() * (10-1) + (1) )}px`;
+            oneBread.style.height = `${Math.floor(Math.random() * (12-1) + (1)  )}px`;
+            oneBread.style.left = `${Math.floor(Math.random() * (60-20) + (20))}%`;
+            console.log(oneBread);
+            breadContainer.appendChild(oneBread);
+        }, 1500)
+
+    }
+
+    console.log(breadContainer);
+}
+
+breadAnimation();
