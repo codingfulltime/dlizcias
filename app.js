e68abd2d3c87cffe412f85fa6d43641d48c29b96
Jeffry Jimenez <codingfulltime@gmail.com>
2020-08-26 17:55:40 -0600

    added services section

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const menuIcon = document.querySelector('.hamburger-menu');
 const mainNav = document.querySelector('.main-nav');
 const navLink = document.querySelectorAll('.nav-link');
-const lineHamburger = document.querySelectorAll('.line');
 
 
 
 // close and open Navbar with Hamburger menu
-menuIcon.addEventListener('click', (e) => {
+menuIcon.addEventListener('click', () => {
     mainNav.classList.toggle('change');
 })
 
@@ -34,7 +33,6 @@ function breadAnimation(){
             console.log(oneBread);
             breadContainer.appendChild(oneBread);
         }, 1500)
-
     }
 }
 
