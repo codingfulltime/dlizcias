68806bccebe27d03f4ba53e5150b3d77e539f12d
Jeffry Jimenez <codingfulltime@gmail.com>
2020-08-25 22:06:08 -0600

    added about section

--- app.js
+++ app.js
@@ -17,39 +17,25 @@ navLink.forEach(function (link){
     })
 })
 
-//hide main-nav on scroll
-let prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-    var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-        mainNav.style.top = "0";
-    } else {
-        mainNav.style.top = "-71.11px";
-    }
-    prevScrollpos = currentScrollPos;
-}
 
 // Bread Animation
 const breadContainer = document.querySelector('.bread-container');
 function breadAnimation(){
 
     let breadItems;
-    for(breadItems = 0; breadItems <= 500; breadItems++) {
+    for( breadItems = 0; breadItems <= 500; breadItems++) {
         setTimeout(function (){
             let oneBread = document.createElement('div');
             oneBread.classList.add('bread');
-            oneBread.classList.add(`bread-`+ breadItems);
             oneBread.style.animationDelay = `${Math.floor(Math.random() * (5-1) + (1) )}s`;
-            oneBread.style.width = `${Math.floor(Math.random() * (10-1) + (1) )}px`;
-            oneBread.style.height = `${Math.floor(Math.random() * (12-1) + (1)  )}px`;
+            oneBread.style.width = `${Math.floor(Math.random() * (8-1) + (1) )}px`;
+            oneBread.style.height = `${Math.floor(Math.random() * (10-1) + (1)  )}px`;
             oneBread.style.left = `${Math.floor(Math.random() * (60-20) + (20))}%`;
             console.log(oneBread);
             breadContainer.appendChild(oneBread);
         }, 1500)
 
     }
-
-    console.log(breadContainer);
 }
 
 breadAnimation();
