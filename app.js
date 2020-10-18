e7ee7df75422d768e8305a080bf840354a17310f
Jeffry Jimenez <codingfulltime@gmail.com>
2020-10-17 18:20:35 -0600

    added new pictures to gallery

--- app.js
+++ app.js
@@ -20,6 +20,28 @@ navLink.forEach(function (link){
     })
 })
 
+// Lazy Loading Images
+const myImages = document.querySelectorAll(`img`);
+const lazyLoad = target => {
+    const io = new IntersectionObserver((entries, observer) => {
+
+        entries.forEach((entry) => {
+
+            if(entry.isIntersecting){
+                const img = entry.target;
+                const src = img.getAttribute('data-lazy');
+
+                img.setAttribute('src', src);
+
+                observer.disconnect();
+            }
+        })
+    });
+
+    io.observe(target);
+}
+
+myImages.forEach(lazyLoad);
 
 // Bread Animation
 const breadContainer = document.querySelector('.bread-container');
